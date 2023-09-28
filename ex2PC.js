d726af30269d8194e73c83fea453a7c1f80a78c8
g00360704 <113925227+g00360704@users.noreply.github.com>
2023-09-28 15:30:27 +0100

    Update ex2PC.js

--- ex2PC.js
+++ ex2PC.js
@@ -1,9 +1,11 @@
 
-//this is an arrow function 
+//this is an arrow function //
 
 const partC = (Val1, Val2)=>{
     console.log(Val1+Val2);
 }
+ //arrow function that takes two number and returns the sum of both numbers. Assigns  arrow function equal to a variable and get the function to output
+to the console//
 
 partC(2,3);
 
